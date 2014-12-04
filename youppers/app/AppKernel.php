90362846c6ac45a93026b5fe18290aa76b0cb055
sergio <sergio@youppers.com>
2014-12-04 14:26:07 +0100

    copiati bundle CompanyBundle e ScraperBundle sonata-project/admin-bundle

--- youppers/app/AppKernel.php
+++ youppers/app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+        	new Youppers\ScraperBundle\YouppersScraperBundle(),
+        	new Youppers\CompanyBundle\YouppersCompanyBundle(),        	
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
