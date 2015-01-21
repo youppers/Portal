61cf9e68ec33579f6c8f99c2d9da89cf211d718c
sergio ufficio <sergio.strampelli@youppers.com>
2015-01-21 19:53:48 +0100

    Working ICS Catalogue loader

--- youppers/src/Youppers/CommonBundle/Command/ICSCatalogueCommand.php
+++ youppers/src/Youppers/CommonBundle/Command/ICSCatalogueCommand.php
@@ -7,6 +7,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Application\Sonata\ClassificationBundle\Entity\Category;
+
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Link;
 
@@ -25,7 +27,11 @@ class ICSCatalogueCommand extends ContainerAwareCommand
 	
 	private $em;
 
-	private $rootCategory;
+	private $update;
+	
+	private $enable;
+	
+	private $rootName;
 	
 	protected function configure()
 	{
@@ -34,34 +40,59 @@ class ICSCatalogueCommand extends ContainerAwareCommand
 		->setDescription('Update catalogue with ICS classification')
 		->addArgument('base', InputArgument::OPTIONAL, 'ISO Standards catalogue uri',$this->icsBaseUri)
 		->addOption('update', null, InputOption::VALUE_NONE, 'Update the database')
-		->addOption('root', null, InputOption::VALUE_REQUIRED, 'Root category','ICS')
+		->addOption('root', null, InputOption::VALUE_REQUIRED, 'Root category name','ICS')
+		->addOption('create', null, InputOption::VALUE_NONE, 'Create the root category if not found')
+		->addOption('enable', null, InputOption::VALUE_NONE, 'Enable categories')
 		;
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$root = $input->getOption("root");
+		$this->rootName = $input->getOption("root");
 		$icsBaseUri = $input->getArgument('base');
-		$output->writeln("Root=".$root." Get ICS from: " . $icsBaseUri);
+		$output->writeln("Root='".$this->rootName."' Get ICS from: " . $icsBaseUri);
 
+		$this->update = $input->getOption('update');
+		$this->enable = $input->getOption('update');
 		
-		$this->em = $this->getContainer()->get('doctrine')->getManager('default');
-		$this->rootCategory == $this->em->getRepository('Application\Sonata\ClassificationBundle\Entity\Category')->findOneBy(array('name' => $root));
-		
-		if ($this->rootCategory == null) {
-			throw new \Exception("Unable to find root category: " . $root);
+		$this->em = $this->getContainer()->get('doctrine')->getManager();
+		$rootCategory = $this->em->getRepository('Application\Sonata\ClassificationBundle\Entity\Category')
+			->findOneBy(array('name' => $this->rootName));
+
+		if ($rootCategory == null && $input->getOption('create')) {
+			$rootCategory = new Category();
+			$rootCategory->setName($this->rootName);
+			$rootCategory->setDescription('International Classification for Standards');
+			$rootCategory->setEnabled($this->enable);
+			$this->em->persist($rootCategory);
+			$this->em->flush();
+			$output->writeln("Created root category with id='".$rootCategory->getId()."'");
 		}
 		
+		if ($rootCategory && $this->update) {
+			$rootCategory->setDescription('International Classification for Standards');
+			$this->em->flush();
+		}
+						
+		if ($rootCategory == null) {
+			throw new \Exception("Unable to find root category: '" . $this->rootName . "'");
+		}
+		
+		$output->writeln("Using root category with id='".$rootCategory->getId()."'");		
+
 		$this->client = new Client();
 		
-		$this->crawl($icsBaseUri, $output);
-		
+		$this->crawl($icsBaseUri, $output, $rootCategory);
+				
 		$output->writeln("Done.");
 	}
 
-	private function crawl($uri, OutputInterface $output, $parent = "") {
+	private function crawl($uri, OutputInterface $output, Category $parentCategory) {
 		$crawler = $this->client->request('GET', $uri);
 		$numRows = $crawler->filterXPath('//*[@id="ics_list"]/tr')->count();
+
+		$parentCategoryChildren = $parentCategory->getChildren();
+		
 		for ($i =1; $i <= $numRows; $i++) {
 			$numTd = $crawler->filterXPath('//*[@id="ics_list"]/tr[' . $i . ']/td')->count();
 			if ($numTd != 2) {
@@ -69,23 +100,60 @@ class ICSCatalogueCommand extends ContainerAwareCommand
 			}				
 			$a = $crawler->filterXPath('//*[@id="ics_list"]/tr[' . $i . ']/td[1]/a');
 			if ($a->count() == 1) {
-				$ics = explode('.',trim($a->text()));
+				$ics = trim($a->text());
 				$link = $a->link();
 			} else {
-				$ics = explode('.',trim($crawler->filterXPath('//*[@id="ics_list"]/tr[' . $i . ']/td[1]')->text()));
+				$ics = trim($crawler->filterXPath('//*[@id="ics_list"]/tr[' . $i . ']/td[1]')->text());
 				$link = null;
 			}
-			$description = trim($crawler->filterXPath('//*[@id="ics_list"]/tr[' . $i . ']/td[2]')->html());
-			$output->writeln($parent . ' Row ' . $i . ' ICS=' . implode('.',$ics) . ' Field=' . $description);
+			$description = $parentCategory->getDescription() . '<br>' . trim($crawler->filterXPath('//*[@id="ics_list"]/tr[' . $i . ']/td[2]')->html());
+				
+			$currentIcs = $this->rootName . '.' . $ics;
+
+			$output->writeln($currentIcs);
+				
+			$currentCategory = null;
+				
+			if ($parentCategoryChildren) {
+				$filteredParentCategoryChildren = $parentCategoryChildren->filter(
+						function($category) use ($currentIcs) {
+							return $category->getName() == $currentIcs;
+						}
+					);
+			}
+									
+			if ($parentCategoryChildren == null || $filteredParentCategoryChildren->count() == 0) {
+				$currentCategory = new Category();
+				$currentCategory->setName($currentIcs);
+				$currentCategory->setDescription($description);
+				$currentCategory->setEnabled($this->enable);
+				$currentCategory->setPosition($i);
+				$this->em->persist($currentCategory);
+				$parentCategory->addChild($currentCategory);
+			} else if ($filteredParentCategoryChildren->count() == 1) {
+				$currentCategory = $filteredParentCategoryChildren->first();
+				if ($this->update) {
+					if ($currentCategory->getDescription() != $description) {			
+						$currentCategory->setDescription($description);
+					}
+					if ($this->enable && !$currentCategory->getEnabled() ) {
+						$currentCategory->setEnabled(true);
+					}
+					if ($currentCategory->getPosition() != $i) {			
+						$currentCategory->setPosition($i);
+					}
+				} 
+			} else {
+				throw new \Exception("More than one category with name '"  . $ics . "'");
+			}
 			
-			$this->save($ics,$description);
-			
-			if ($link && count($ics) < 3) {
-				$this->crawl($link->getUri(), $output,implode('.',$ics));
+			if ($link && count(explode('.',$ics)) < 3) {
+				$this->crawl($link->getUri(), $output, $currentCategory);
 			}			
 		}
+		
+		$this->em->flush();
+		
 	}
 	
-	private function save($ics,$description) {
-	}	
 }
