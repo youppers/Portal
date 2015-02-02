0346fe8d0eab2933fdd28d6331ef0ef2e3345d95
sergio ufficio <sergio.strampelli@youppers.com>
2015-02-02 21:30:37 +0100

    WIP YouppersProduct

---
+++ youppers/src/Youppers/ProductBundle/Entity/ProductType.php
@@ -0,0 +1,249 @@
+<?php
+namespace Youppers\ProductBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="youppers__product_type")
+ * @ORM\HasLifecycleCallbacks
+ */
+class ProductType
+{
+	public function __toString()
+	{
+		return $this->getName() ?: 'New';
+	}
+	
+	/**
+	 * @ORM\Column(type="guid")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="UUID")
+	 */
+	protected $id;
+			
+	/**
+	 * @ORM\Column(type="string", length=60, unique=true)
+	 */
+	protected $name;
+
+	/**
+	 * @ORM\Column(name="code", type="string", length=60, unique=true)
+	 */
+	protected $code;
+	
+	/**
+	 * @ORM\Column(type="boolean", options={"default":true})
+	 */
+	protected $enabled;
+		
+	/**
+	 * @ORM\Column(type="text", nullable=true )
+	 */
+	protected $description;
+	
+	/**
+	 * @ORM\Column(type="string", nullable=true)
+	 */
+	protected $className;
+	
+	/**
+	 * @ORM\Column(type="datetime", name="updated_at")
+	 */
+	protected $updatedAt;
+	
+	/**
+	 * @ORM\Column(type="datetime", name="created_at")
+	 */
+	protected $createdAt;
+			
+	/**
+	 * @ORM\PrePersist()
+	 */
+	public function prePersist()
+	{
+		$this->createdAt = new \DateTime();
+		$this->updatedAt = new \DateTime();
+	}
+
+	/**
+	 * @ORM\PreUpdate()
+	 */
+	public function preUpdate()
+	{
+		$this->updatedAt = new \DateTime();
+	}	
+	
+	// php app/console doctrine:generate:entities --no-backup YouppersProductBundle
+
+    /**
+     * Get id
+     *
+     * @return guid 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return ProductType
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return ProductType
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set enabled
+     *
+     * @param boolean $enabled
+     * @return ProductType
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+
+        return $this;
+    }
+
+    /**
+     * Get enabled
+     *
+     * @return boolean 
+     */
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return ProductType
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set className
+     *
+     * @param string $className
+     * @return ProductType
+     */
+    public function setClassName($className)
+    {
+        $this->className = $className;
+
+        return $this;
+    }
+
+    /**
+     * Get className
+     *
+     * @return string 
+     */
+    public function getClassName()
+    {
+        return $this->className;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return ProductType
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return ProductType
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
---
+++ youppers/src/Youppers/ProductBundle/YouppersProductBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Youppers\ProductBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YouppersProductBundle extends Bundle
+{
+}
