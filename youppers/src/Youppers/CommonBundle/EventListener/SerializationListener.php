952522f4a834e9a5df36a33f71af930c0137b859
sergio su silver <sergio.strampelli@youppers.com>
2015-03-02 19:27:47 +0100

    ad "url" to serialized images

---
+++ youppers/src/Youppers/CommonBundle/EventListener/SerializationListener.php
@@ -0,0 +1,40 @@
+<?php
+namespace Youppers\CommonBundle\EventListener;
+
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+
+/**
+ * Add data after serialization
+ *
+ * @Service("axo.listener.serializationlistener")
+ * @Tag("jms_serializer.event_subscriber")
+ */
+class SerializationListener implements EventSubscriberInterface
+{
+	private $defaultFormat = 'list'; // TODO da rendere configurabile
+
+	/**
+	 * @inheritdoc
+	 */
+	static public function getSubscribedEvents()
+	{
+		return array(
+				array('event' => 'serializer.post_serialize', 'class' => 'Application\Sonata\MediaBundle\Entity\Media', 'method' => 'onPostSerialize'),
+		);
+	}
+
+	public function onPostSerialize(ObjectEvent $event)
+	{
+		global $kernel;
+		$imageProvider = $kernel->getContainer()->get('sonata.media.provider.image');
+		$image = $event->getObject();
+		$formats = $imageProvider->getFormats();
+		if (array_key_exists($image->getContext() . '_' . $this->defaultFormat,$formats)) {
+			$format = $imageProvider->getFormatName($image, $this->defaultFormat);
+		} else {
+			$format = 'reference';
+		}
+		$event->getVisitor()->addData('url',$imageProvider->generatePublicUrl($image, $format));
+	}
+}
