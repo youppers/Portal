2626889ba8c15470b7107cc4c70aabfc40402e23
sergio su silver <sergio.strampelli@youppers.com>
2015-03-30 13:36:07 +0200

    debug validate variant

--- youppers/src/Youppers/ProductBundle/Entity/ProductVariant.php
+++ youppers/src/Youppers/ProductBundle/Entity/ProductVariant.php
@@ -15,6 +15,7 @@ use Youppers\ProductBundle\Entity\ProductCollection;
  * @ORM\HasLifecycleCallbacks
  * @ConsistentBrand()
  * @Serializer\ExclusionPolicy("all") 
+ * @Validator\UniqueEntity({"product","productCollection"})
  */
 class ProductVariant
 {
