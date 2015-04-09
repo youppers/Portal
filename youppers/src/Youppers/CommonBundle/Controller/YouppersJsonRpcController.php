9858157cd2883b96724258613c8a061bd5dff522
sergio ufficio <sergio.strampelli@youppers.com>
2015-04-09 17:31:11 +0200

    logger of request / response for json-rpc

---
+++ youppers/src/Youppers/CommonBundle/Controller/YouppersJsonRpcController.php
@@ -0,0 +1,21 @@
+<?php
+
+use Wa72\JsonRpcBundle\Controller\JsonRpcController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class YouppersJsonRpcController extends JsonRpcController
+{
+	public function execute(Request $httprequest)
+	{
+		$logger = $this->container->get('logger');
+		
+		$logger->info(sprintf("JSON-RPC Request: %s",$httprequest->getContent()));
+		
+		$response = parent::execute($httprequest);
+		
+		$logger->info(sprintf("JSON-RPC Response: %s",$response->getContent()));
+		
+		return $response;
+	}
+} 
