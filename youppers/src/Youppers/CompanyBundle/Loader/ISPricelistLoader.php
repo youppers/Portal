5ead0e252fd85b9778b32aeee23cb81fab132502
sergio ufficio <sergio.strampelli@youppers.com>
2015-04-09 12:23:41 +0200

    price cleaning is default (in code)

--- youppers/src/Youppers/CompanyBundle/Loader/ISPricelistLoader.php
+++ youppers/src/Youppers/CompanyBundle/Loader/ISPricelistLoader.php
@@ -10,7 +10,7 @@ class ISPricelistLoader extends AbstractPricelistLoader
 			'brand' => function($row) { $matches=preg_split("/\-/",$row['CDS Italia']); if (array_key_exists(1,$matches)) return $matches[1]; },
 			'name' => 'Description',
 			'gtin' => 'EAN13',
-			'price' => function($row) { return strtr($row['Price'],array(" " => "", "€" => "","." => "","," => ".")); },
+			'price' => 'Price',
 			'collection' => 'Suite',
 			'type' => 'Categoria',	
 			'uom' => function($row) { return 'PCE'; }
