e3cee13671085240cecc96957dd32f01f159f9b1
Sergio su Poldo <sergio.strampeli@youppers.com>
2015-05-03 19:33:03 +0200

    moved some test methods for json in dedicated superclass

---
+++ youppers/src/Youppers/CommonBundle/Tests/JsonTestCase.php
@@ -0,0 +1,127 @@
+<?php
+namespace Youppers\CommonBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Cmf\Component\Routing\ChainRouter;
+use Youppers\CommonBundle\Controller\YouppersJsonRpcController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\DomCrawler\Crawler;
+use Sonata\UserBundle\Entity\UserManager;
+
+class JsonTestCase extends WebTestCase
+{
+	
+	protected $credentials = array('name' => 'test', 'username' => 'signoramaria', 'password' => 'signoramaria');
+	
+	protected $jsonRpcEndpoint = '/jsonrpc/';
+
+	private static $seq = 0;
+	
+	private static $auth;
+		
+	public function setUp()
+	{
+		static::bootKernel();		
+		$this->controller = static::$kernel->getContainer()->get('youppers_common.jsonrpccontroller');
+		$this->logger = static::$kernel->getContainer()->get('logger');
+		self::$seq = rand();
+	}	
+	
+	protected function get($what)
+	{
+		return static::$kernel->getContainer()->get($what);
+	}
+
+	protected function getAccessToken()
+	{
+		if (!isset(self::$auth)) {
+			$client = self::createClient();
+			
+			$oauthClientManager = $client->getContainer()->get('fos_oauth_server.client_manager');		
+			$oauthClient = $oauthClientManager->findClientBy(array('name' => $this->credentials['name']));
+
+			$url = $client->getContainer()->get('router')->generate('fos_oauth_server_token');
+
+			$params = array(
+						'client_id' => $oauthClient->getPublicId(),
+						'client_secret' => $oauthClient->getSecret(),
+						'grant_type' => 'client_credentials',
+					);
+			
+			if (array_key_exists('username',$this->credentials)) {
+				$params['grant_type'] = 'password';
+				$params['username'] = $this->credentials['username'];
+				$params['password'] = $this->credentials['password'];
+			}
+				
+			$client->request('POST', $url, $params);
+	
+			$this->assertEquals(
+	    		200, // o Symfony\Component\HttpFoundation\Response::HTTP_OK
+	    		$client->getResponse()->getStatusCode(),
+				'Autentication failed'				
+			);
+	
+			self::$auth = json_decode($client->getResponse()->getContent());
+			$this->logger->info("Got access_token " . self::$auth->access_token);
+		}		
+		return self::$auth->access_token;
+	}
+	
+	protected function makeMethodRequest($method,$params = array())
+	{
+		$requestdata = array(
+				'jsonrpc' => '2.0',
+				'id' => ++self::$seq,
+				'method' => $method,
+				'params' => $params
+		);
+		$response = $this->makeRequest($requestdata);
+		return $response;
+	}
+	
+	private function makeRequest($requestdata)
+	{
+		/** @var \JMS\Serializer\Serializer $serializer */
+		$serializer = static::$kernel->getContainer()->get('jms_serializer');
+		return json_decode($this->controller->execute(
+				new Request(array(), array(), array(), array(), array(), array(), $serializer->serialize($requestdata, 'json'))
+		)->getContent(), true);
+	}
+	
+	protected function makeHttpMethodRequest($method,$params = array())
+	{
+		$requestdata = array(
+				'jsonrpc' => '2.0',
+				'id' => ++self::$seq,
+				'method' => $method,
+				'params' => $params
+		);
+		$response = $this->makeHttpRequest($requestdata);
+		return json_decode($response->getContent(), true);
+	}
+	
+	private function makeHttpRequest($requestdata)
+	{
+		/** @var \JMS\Serializer\Serializer $serializer */
+		$serializer = static::$kernel->getContainer()->get('jms_serializer');
+		$client = static::createClient();
+		//dump($client);
+		$client->request('GET',$this->jsonRpcEndpoint,
+				array( // parameters
+						'access_token' => $this->getAccessToken(),
+				),
+				array(), // files
+				array( // The server parameters (HTTP headers are referenced with a HTTP_ prefix as PHP does)
+						'HTTP_HEADERS' => array(
+								'Accept'     => 'application/json',
+								'Content-Type' => 'application/json',
+						)
+				),
+				$serializer->serialize($requestdata, 'json') // The raw body data
+		);
+		return $client->getResponse();
+	}
+
+}
--- youppers/src/Youppers/CustomerBundle/Tests/Controller/ProfileServiceTest.php
+++ youppers/src/Youppers/CustomerBundle/Tests/Controller/ProfileServiceTest.php
@@ -1,132 +1,19 @@
 <?php
 namespace Youppers\CustomerBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Cmf\Component\Routing\ChainRouter;
-use Youppers\CommonBundle\Controller\YouppersJsonRpcController;
-use Symfony\Component\HttpFoundation\Request;
+use Youppers\CommonBundle\Tests\JsonTestCase;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\DomCrawler\Crawler;
-use Sonata\UserBundle\Entity\UserManager;
 
-class ProfileServiceTest extends WebTestCase
+class ProfileServiceTest extends JsonTestCase
 {
 	
-	private $usercredentials = array('username' => 'raf', 'password' => 'nomos');
-	
 	private $otherUserId = '1';
-	
-	private $controller;  // json controller
-	
-	public function setUp()
-	{
-		static::bootKernel();		
-		$this->controller = static::$kernel->getContainer()->get('youppers_common.jsonrpccontroller');
-		$this->logger = static::$kernel->getContainer()->get('logger');
-	}	
-	
-	private function get($what)
-	{
-		return static::$kernel->getContainer()->get($what);
-	}
-
-	private static $auth;
-	
-	private function getPasswordAccessToken()
-	{
-		if (!isset(self::$auth)) {
-			$client = self::createClient();
-			
-			$oauthClientManager = $client->getContainer()->get('fos_oauth_server.client_manager');		
-			$oauthClient = $oauthClientManager->findClientBy(array('name' => 'test'));
-
-			$url = $client->getContainer()->get('router')->generate('fos_oauth_server_token');		
-			$client->request('POST', $url,array(
-					'client_id' => $oauthClient->getPublicId(),
-					'client_secret' => $oauthClient->getSecret(),
-					'grant_type' => 'password',
-					'username' => $this->usercredentials['username'],
-					'password' => $this->usercredentials['password'],
-			));
-	
-			$this->assertEquals(
-	    		200, // o Symfony\Component\HttpFoundation\Response::HTTP_OK
-	    		$client->getResponse()->getStatusCode(),
-				'User autentication failed'				
-			);
-	
-			self::$auth = json_decode($client->getResponse()->getContent());
-			$this->logger->info("Got access_token " . self::$auth->access_token);
-		}		
-		return self::$auth->access_token;
-	}
-	
- 	private function jsonRpcEndpoint() {
- 		return '/jsonrpc/';
-	}
- 	
-	private static $seq = 0;
-	
-	private function makeMethodRequest($method,$params = array())
-	{
-		$requestdata = array(
-				'jsonrpc' => '2.0',
-				'id' => ++self::$seq,
-				'method' => $method,
-				'params' => $params
-		);
-		$response = $this->makeRequest($requestdata);
-		return $response;
-	}
-	
-	private function makeRequest($requestdata)
-	{
-		/** @var \JMS\Serializer\Serializer $serializer */
-		$serializer = static::$kernel->getContainer()->get('jms_serializer');
-		return json_decode($this->controller->execute(
-				new Request(array(), array(), array(), array(), array(), array(), $serializer->serialize($requestdata, 'json'))
-		)->getContent(), true);
-	}
-	
-	private function makeHttpMethodRequest($method,$params = array())
-	{
-		$requestdata = array(
-				'jsonrpc' => '2.0',
-				'id' => ++self::$seq,
-				'method' => $method,
-				'params' => $params
-		);
-		$response = $this->makeHttpRequest($requestdata);
-		return json_decode($response->getContent(), true);
-	}
-	
-	private function makeHttpRequest($requestdata)
-	{
-		/** @var \JMS\Serializer\Serializer $serializer */
-		$serializer = static::$kernel->getContainer()->get('jms_serializer');
-		$client = static::createClient();
-		//dump($client);
-		$client->request('GET',$this->jsonRpcEndpoint(),
-				array( // parameters
-						'access_token' => $this->getPasswordAccessToken(),
-				),
-				array(), // files
-				array( // The server parameters (HTTP headers are referenced with a HTTP_ prefix as PHP does)
-						'HTTP_HEADERS' => array(
-								'Accept'     => 'application/json',
-								'Content-Type' => 'application/json',
-						)
-				),
-				$serializer->serialize($requestdata, 'json') // The raw body data
-		);
-		return $client->getResponse();
-	}
-		
+				
 	public function testAuth()
 	{
 		$client = static::createClient();
 	
-		$client->request('GET', $this->jsonRpcEndpoint());
+		$client->request('GET', $this->jsonRpcEndpoint);
 	
 		$this->assertEquals(
 				Response::HTTP_UNAUTHORIZED,
@@ -134,7 +21,7 @@ class ProfileServiceTest extends WebTestCase
 				'Access without access_token must fail'
 		);
 	
-		$client->request('GET', $this->jsonRpcEndpoint(), array('access_token' => $this->getPasswordAccessToken()));
+		$client->request('GET', $this->jsonRpcEndpoint, array('access_token' => $this->getAccessToken()));
 	
 		$this->assertEquals(
 				Response::HTTP_OK,
