f57e73e95db5c19adbe6cc0be7cae1fcb77823fd
sergio ufficio <sergio.strampelli@youppers.com>
2015-05-21 12:07:40 +0200

    ELIOS pricelist

---
+++ youppers/src/Youppers/CompanyBundle/Loader/ELIOS/PricelistLoader.php
@@ -0,0 +1,43 @@
+<?php
+namespace Youppers\CompanyBundle\Loader\ELIOS;
+
+use Youppers\CompanyBundle\Entity\Brand;
+use Youppers\CompanyBundle\Loader\AbstractPricelistCollectionLoader;
+use Youppers\CompanyBundle\Loader\LoaderMapper;
+
+class PricelistLoader extends AbstractPricelistCollectionLoader
+{
+	public function createMapper()
+	{
+		$mapping = array(
+            'brand' => 'MARCHIO',
+			'code' => 'CODICE',
+			'name' => function($row) {
+                $name = $row['ARTICOLO'];
+				$dim = $row['FORMATO'];
+				if (stripos($name,$dim) === false) {
+					$name .= ' {' . $dim . '}';
+				}
+				return $name;
+			}, 
+			'collection' => 'SERIE',
+			'uom' => 'UMV',
+            'gtin' => 'APFBCO',
+            'price' => 'LISTINO'
+		);
+		$mapper = new LoaderMapper($mapping);
+		return $mapper;
+	}
+	
+	private $newCollectionProductType;
+	
+	protected function getNewCollectionProductType(Brand $brand, $code)
+	{
+		if (!isset($this->newCollectionProductType)) {
+			$this->newCollectionProductType= $this->productTypeManager
+				->findOneBy(array('code' => 'TILE'));
+		}
+		return $this->newCollectionProductType;
+	}
+	
+}
