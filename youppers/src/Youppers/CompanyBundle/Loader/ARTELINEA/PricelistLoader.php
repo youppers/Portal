ffd57aa29cedc6fcc07f91c2cf0872ef7740c536
sergio ufficio <sergio.strampelli@youppers.com>
2015-05-29 14:40:12 +0200

    WIP loader Artelinea

--- youppers/src/Youppers/CompanyBundle/Loader/ARTELINEA/PricelistLoader.php
+++ youppers/src/Youppers/CompanyBundle/Loader/ARTELINEA/PricelistLoader.php
@@ -7,6 +7,66 @@ use Youppers\CompanyBundle\Loader\LoaderMapper;
 
 class PricelistLoader extends AbstractPricelistCollectionLoader
 {
+
+    static $collections = array(
+        'BTRG' => 'Regolo', // basin top
+        'BTDC' => 'Decor', // basin top
+        'BTM\.' => 'Top', // basin top su misura
+        'KZDC' => 'Decor', // alzatine opalite
+        'KYDC' => 'Decor', // alzatine opalite
+        'PAMT' => 'Metafora', // Panca
+        'WMV' => 'Move', // lighting
+        'BTA' => 'Top', // basin top su misura angolare
+        'BTM' => 'Monolite 2.0', // basin top
+        'KRG' => 'Regolo', // opalite
+        'KDC' => 'Decor', // opalite
+        'KKM' => 'Kimono', // opalite
+        'KMF' => 'Metafora', // opalite
+        'TMV' => 'Move', // specchi
+        'LRG' => 'Regolo', // lighting
+        'TBR' => 'Broadway', // specchi
+        'TBX' => 'Box', // specchi
+        'TBC' => 'Barcode', // specchi
+        'TBA' => 'Dado', // specchi
+        'TDE' => 'Deco', // specchi
+        'BTU' => 'Uno', // basin top
+        'UNO' => 'Uno',
+        'WL' => 'Lavorazioni',
+        'ML' => 'Lavorazioni',
+        'SH' => 'Shock',
+        'BB' => 'Basin Box', // lavabi cristallo
+        'BT' => 'Vision', // basin top
+        'KM' => 'Kimono',
+        'MF' => 'Metafora',
+        'AT' => 'Atollo',
+        'FR' => 'Frammento',
+        'OM' => 'Tormento', // opalite o ceramica
+        'OP' => 'Opus',
+        'TT' => 'Tormento',
+        'TL' => 'Led', // specchi
+        'TA' => 'Argento', // specchi
+        'TF' => 'Flash', // specchi
+        'TO' => 'Oblo', // specchi
+        'TS' => 'Swing', // specchi
+        'TB' => 'I Borgia', // specchi
+        'BG' => 'I Borgia',
+        'DC' => 'Decor',
+        'SM' => 'Summa',
+        'M2' => 'Monolite 2.0',
+        'VR' => 'Vero',
+        'DM' => 'Domino',
+        'DQ' => 'Domino 44',
+        'VT' => 'Vetrinette',
+        'K\.' => 'Cubic Fly Vision', // staffe
+        'T' => 'Box', // specchi
+        'R' => 'Ricambi', // varie collezioni
+        'Q' => 'Quadra', // staffe
+        'B' => 'Basin', // lavabi
+        'C' => 'Colonne',
+        'K' => 'Top', // top su misura
+        'F' => 'Top', // lavorazioni particolari top
+    );
+
 	public function createMapper()
 	{
 		$mapping = array(
@@ -14,8 +74,16 @@ class PricelistLoader extends AbstractPricelistCollectionLoader
                 $code = trim($row['Codice Articolo']);
                 $marca = trim($row['Marca / Descrizione']);
                 if (!empty($marca)) {
-                    $marca = preg_replace("/\ /","-",$marca);;
-                    $code .= '-' . trim($marca);
+                    $marca = preg_replace('/[^a-z_\-0-9]/i',' ',$marca);
+                    foreach(explode(" ",$marca) as $atom) {
+                        $atom = trim($atom);
+                        if (empty($atom)) {
+                            continue;
+                        }
+                        $code .= '-' . substr($atom,0,3);
+                    }
+                    //$marca = preg_replace("/\ /","-",$marca);;
+                    //$code .= '-' . trim($marca);
                 }
                 return $code;
             },
@@ -28,7 +96,17 @@ class PricelistLoader extends AbstractPricelistCollectionLoader
 				return $name;
 			}, 
 			'uom' => function($row) { return 'PCE'; },
-            'price' => 'Prezzo Listino'
+            'price' => 'Prezzo Listino',
+            'collection' => function($row) {
+                $articolo = trim($row['Codice Articolo']);
+                foreach (self::$collections as $id => $code) {
+                    if (preg_match('/^' .$id .'/',$articolo)) {
+                        return $code;
+                    }
+                }
+                return "default";
+            },
+
 		);
 		$mapper = new LoaderMapper($mapping);
 		return $mapper;
