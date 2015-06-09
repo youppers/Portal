5361732ae0edcb8ee1434a6986dd98b92adf7e55
Sergio siteground <sergio@ascia.it>
2015-06-09 14:16:43 -0500

    Regenerate thumbnails

---
+++ youppers/thumbnails.sh
@@ -0,0 +1,6 @@
+php app/console sonata:media:sync-thumbnails sonata.media.provider.image youppers_company_logo 
+php app/console sonata:media:sync-thumbnails sonata.media.provider.image youppers_brand_logo
+php app/console sonata:media:sync-thumbnails sonata.media.provider.image youppers_product
+php app/console sonata:media:sync-thumbnails sonata.media.provider.image youppers_attribute
+php app/console sonata:media:sync-thumbnails sonata.media.provider.image youppers_box
+php app/console sonata:media:sync-thumbnails sonata.media.provider.image youppers_consultant_photo
