c1cb1717ef5fe2d1a08006f5df2febf2de52d18c
Sergio su Poldo <sergio.strampeli@youppers.com>
2015-06-26 20:23:43 +0200

    test Session.update

--- youppers/src/Youppers/CustomerBundle/Tests/Controller/SessionServiceTest.php
+++ youppers/src/Youppers/CustomerBundle/Tests/Controller/SessionServiceTest.php
@@ -9,18 +9,32 @@ class SessionServiceTest extends JsonTestCase
 	
 	private static $sessionId;
 
-	public function testSessionNew()
-	{
-		$response = $this->makeHttpMethodRequest('Session.new');
-		//dump($response);
-		$this->assertGreaterThan(
-				0,
-				count($response['result'])
-		);		
-		self::$sessionId = $response['result']['id'];
-	}
-	
-	public function testSessionRead()
+    public function testSessionNew()
+    {
+        $response = $this->makeHttpMethodRequest('Session.new');
+        //dump($response);
+        $this->assertGreaterThan(
+            0,
+            count($response['result'])
+        );
+        self::$sessionId = $response['result']['id'];
+    }
+
+    public function testSessionEditName()
+    {
+        $name = 'Visita ' . md5(rand());
+        $response = $this->makeHttpMethodRequest('Session.update', array('sessionId' => self::$sessionId, 'data' => array('name' => $name)));
+        $this->assertGreaterThan(
+            0,
+            count($response['result'])
+        );
+        $this->assertEquals(
+            $name,
+            $response['result']['name']
+        );
+    }
+
+    public function testSessionRead()
 	{
 		$response = $this->makeHttpMethodRequest('Session.read',array('sessionId' => self::$sessionId));		
 		$this->assertGreaterThan(
