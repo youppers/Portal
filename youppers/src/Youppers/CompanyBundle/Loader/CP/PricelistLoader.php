e9d9c1f1c490db73a671fcd263598b758a688550
Sergio su xbuntu su poldo <sergio.strampelli@youppers.com>
2016-02-03 23:19:08 +0100

    casalgrande padana loader

--- youppers/src/Youppers/CompanyBundle/Loader/CP/PricelistLoader.php
+++ youppers/src/Youppers/CompanyBundle/Loader/CP/PricelistLoader.php
@@ -11,27 +11,25 @@ class PricelistLoader extends AbstractPricelistLoader
 	{
 		$mapping = array(
             // 'brand' => 'MARCHIO',  // Brand unico, specificare il brand usando "loader CP CP"
-			'code' => 'ARTICOLO',
-			'name' => function($row) {
+			self::FIELD_CODE => 'ARTICOLO',
+			self::FIELD_NAME => function($row,&$data) {
+				$formato = $row['FORMATO'];
+				$m = array();
+				if (preg_match("/^([0-9,]+)\s*x\s*([0-9,]+)/i",$formato,$m)) {
+					$data['DIM'] = $m[1] . 'x' . $m[2];
+				}
+				if (preg_match("/^([0-9,]+)\s*x\s*([0-9,]+)\s+([0-9,]+)/i",$formato,$m)) {
+					$data['THICKNESS'] = $m[3];
+				}
                 return $row['SERIE'] . ' ' . $row['DESCRIZIONE'] . ' ' . $row['FORMATO'];
 			},
-			'collection' => 'SERIE',
-			'uom' => 'UM',
-            'price' => '1A SC.'
+			self::FIELD_COLLECTION => 'SERIE',
+			self::FIELD_UOM => 'UM',
+			self::FIELD_PRICE => '1A SC.',
+			'prova' => 123
 		);
 		$mapper = new LoaderMapper($mapping);
 		return $mapper;
 	}
-	
-	private $newCollectionProductType;
-	
-	protected function getNewCollectionProductType(Brand $brand, $code)
-	{
-		if (!isset($this->newCollectionProductType)) {
-			$this->newCollectionProductType= $this->productTypeManager
-				->findOneBy(array('code' => 'TILE'));
-		}
-		return $this->newCollectionProductType;
-	}
-	
+
 }
