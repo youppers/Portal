0147e83bcd3249b1a6050e7e39cc4becb35b2631
sergio <sergio.strampelli@youppers.com>
2016-03-30 11:38:09 +0200

    product manager has method for searching products that use a cache for new products not persisted

--- youppers/src/Youppers/CompanyBundle/Manager/ProductManager.php
+++ youppers/src/Youppers/CompanyBundle/Manager/ProductManager.php
@@ -5,16 +5,26 @@ namespace Youppers\CompanyBundle\Manager;
 use Sonata\CoreBundle\Model\BaseEntityManager;
 use Sonata\CoreBundle\Model\ManagerInterface;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Youppers\CompanyBundle\Entity\Brand;
+use Youppers\CompanyBundle\Entity\Product;
 
 class ProductManager extends BaseEntityManager
 {
 
     private $gtinCache = array();
 
+    private $productCache = array();
+
     public function save($entity, $andFlush = true) {
         $gtin = $entity->getGtin();
         if ($gtin != null) {
             $this->gtinCache[$gtin] = $entity;
+        }
+        if ($andFlush) {
+            $this->productCache = array();
+        } else {
+            $key = $entity->getBrand()->getCode() . '-' . $entity->getCode();
+            $this->productCache[$key] = $entity;
         }
         return parent::save($entity,$andFlush);
     }
@@ -29,6 +39,15 @@ class ProductManager extends BaseEntityManager
             }
             return $product;
         }
+    }
+
+    public function findOneByBrandAndCode(Brand $brand, $productCode)
+    {
+        $key = $brand->getCode() . '-' . $productCode;
+        if (array_key_exists($key,$this->productCache)) {
+            return $this->productCache[$key];
+        }
+        return $this->findOneBy(array('brand' => $brand, 'code' => $productCode));
     }
 		
 }
