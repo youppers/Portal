f4da9b05553686afe550be986193084e8982f90d
sergio <sergio.strampelli@youppers.com>
2016-04-07 16:59:15 +0200

    Marazzi guesser: auto add standard for DIM

--- youppers/src/Youppers/ProductBundle/Guesser/MZ/VariantGuesser.php
+++ youppers/src/Youppers/ProductBundle/Guesser/MZ/VariantGuesser.php
@@ -1,9 +1,29 @@
 <?php
 namespace Youppers\ProductBundle\Guesser\MZ;
 
+use Youppers\ProductBundle\Entity\AttributeType;
+use Youppers\ProductBundle\Entity\ProductCollection;
+use Youppers\ProductBundle\Guesser\BasePropertyGuesser;
 use Youppers\ProductBundle\Guesser\BaseVariantGuesser;
 
 class VariantGuesser extends BaseVariantGuesser
 {
+    protected function getCollectionTypeGuesser(ProductCollection $collection, AttributeType $type)
+    {
+        if ($type->getCode() == 'DIM') {
+            return new DimPropertyGuesser($type, $this->variantPropertyManager, $this->attributeOptionManager);
+        }
+        return parent::getCollectionTypeGuesser($collection, $type);
+    }
+
+}
+
+class DimPropertyGuesser extends BasePropertyGuesser
+{
+
+    public function getDefaultStandardName()
+    {
+        return 'Lato x Lato in mm';
+    }
 
 }
