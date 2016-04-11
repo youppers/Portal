23248c3a265f1325c3d155796c26a2167619f636
sergio <sergio.strampelli@youppers.com>
2016-04-11 23:30:33 +0200

    product price with cache of new enities

--- youppers/src/Youppers/CompanyBundle/Manager/ProductPriceManager.php
+++ youppers/src/Youppers/CompanyBundle/Manager/ProductPriceManager.php
@@ -3,10 +3,37 @@
 namespace Youppers\CompanyBundle\Manager;
 
 use Sonata\CoreBundle\Model\BaseEntityManager;
-use Sonata\CoreBundle\Model\ManagerInterface;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Youppers\CompanyBundle\Entity\Product;
+use Youppers\CompanyBundle\Entity\Pricelist;
 
 class ProductPriceManager extends BaseEntityManager
 {
+
+    private $priceCache = array();
+
+    public function save($entity, $andFlush = true) {
+        if ($andFlush) {
+            $this->priceCache = array();
+        } else {
+            $key = $entity->getPricelist()->getCode() . '-' . $entity->getProduct()->getCode();
+            $this->priceCache[$key] = $entity;
+        }
+        return parent::save($entity,$andFlush);
+    }
+
+    public function flush()
+    {
+        $this->getEntityManager()->flush();
+        $this->priceCache = array();
+    }
+
+    public function findOneByProductAndList(Product $product, Pricelist $pricelist)
+    {
+        $key = $pricelist->getCode() . '-' . $product->getCode();
+        if (array_key_exists($key,$this->priceCache)) {
+            return $this->priceCache[$key];
+        }
+        return $this->findOneBy(array('product' => $product, 'pricelist' => $pricelist));
+    }
 
 }
