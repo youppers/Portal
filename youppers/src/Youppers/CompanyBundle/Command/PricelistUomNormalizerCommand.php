046574288cb9a290cc953139a0e25d33acbbe343
sergio <sergio.strampelli@youppers.com>
2016-04-12 10:41:13 +0200

    reorganized code of normalizer for speed and memory usage

--- youppers/src/Youppers/CompanyBundle/Command/PricelistUomNormalizerCommand.php
+++ youppers/src/Youppers/CompanyBundle/Command/PricelistUomNormalizerCommand.php
@@ -19,9 +19,15 @@ class PricelistUomNormalizerCommand extends ContainerAwareCommand
 		$this
 			->setName('youppers:pricelist:uom:normalize')
 			->setDescription('Normalize UOM in Pricelist')
-			->addArgument('pricelist', InputArgument::OPTIONAL, 'Code of the pricelist to update')
+			->addArgument('pricelist', InputArgument::IS_ARRAY, 'Codes of the pricelist to update')
 		;
 	}
+
+    /** @var OutputInterface  */
+    private $output;
+
+    /** @var PricelistManager $pricelistManager */
+    private $pricelistManager;
 
 	/**
 	 * @param InputInterface $input
@@ -31,58 +37,65 @@ class PricelistUomNormalizerCommand extends ContainerAwareCommand
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		$input->validate();
+        $this->output = $output;
 
-		/** @var Pricelist $pricelist */
-		$this->normalize($input->getArgument("pricelist"),$output);
+        $this->pricelistManager = $this->getContainer()->get('youppers.company.manager.pricelist');
 
+
+        $codes = $input->getArgument("pricelist");
+        if (count($codes) == 0) {
+            $this->normalizeAll();
+        } else {
+            foreach ($codes as $code) {
+                $this->normalize($code);
+            }
+        }
 	}
 
-	protected function normalize($pricelistcode,OutputInterface $output)
+    protected function normalizeAll()
     {
-        /** @var PricelistManager $pricelistManager */
-        $pricelistManager = $this->getContainer()->get('youppers.company.manager.pricelist');
+        $pricelists = $this->pricelistManager->findBy(array('enabled' => true));
+        $codes = array();
+        foreach ($pricelists as $pricelist) {
+            $this->output->writeln(sprintf("Pricelist '%s'",$pricelist));
+            $codes[] = $pricelist->getCode();
+        }
+        $this->pricelistManager->getObjectManager()->clear();
+        foreach ($codes as $code) {
+            $this->normalize($code);
+        }
+    }
 
-        /** @var ObjectManager $pricelistObjectManager */
-        $pricelistObjectManager = $pricelistManager->getObjectManager();
-
-		if (empty($pricelistcode)) {
-            $criteria = array('enabled' => true);
-            $pricelists = $pricelistManager->findBy($criteria);
-            foreach ($pricelists as $pricelist) {
-                $output->writeln(sprintf("Pricelist '%s'",$pricelist));
-                $this->normalize($pricelist->getCode(),$output);
+	protected function normalize($pricelistcode)
+    {
+        $pricelist = $this->pricelistManager->findOneBy(array('code' => $pricelistcode));
+        /** @var ProductPriceManager $productPriceManager */
+        $productPriceManager = $this->getContainer()->get('youppers.company.manager.productprice');
+        $prices = $productPriceManager->findBy(array('pricelist' => $pricelist));
+        $i = 0;
+        $iprec = 1;
+        $k = 0;
+        $n = count($prices);
+        $this->output->writeln(sprintf("Pricelist '%s' normalizing %d prices",$pricelist,$n));
+        foreach ($prices as $price) {
+            $k++;
+            $oldUom = $price->getUom();
+            $newUom = UomChoiceList::normalize($oldUom);
+            if ($newUom != $oldUom) {
+                $price->setUom($newUom);
+                $i++;
             }
-		} else {
-            $criteria['code'] = $pricelistcode;
-            $pricelist = $pricelistManager->findOneBy($criteria);
-            /** @var ProductPriceManager $productPriceManager */
-            $productPriceManager = $this->getContainer()->get('youppers.company.manager.productprice');
-			$prices = $productPriceManager->findBy(array('pricelist' => $pricelist));
-			$i = 0;
-            $iprec = 1;
-			$k = 0;
-			$n = count($prices);
-			$output->writeln(sprintf("Pricelist '%s' normalizing %d prices",$pricelist,$n));
-			foreach ($prices as $price) {
-				$k++;
-				$oldUom = $price->getUom();
-				$newUom = UomChoiceList::normalize($oldUom);
-				if ($newUom != $oldUom) {
-					$price->setUom($newUom);
-					$i++;
-				}
-				if ((($i > $iprec ) && ($i % 500) === 0) || ($k == $n)) {
-                    $iprec = $i;
-					$pricelistObjectManager->flush();
-					$output->writeln(sprintf("Updated %d uom of %d/%d prices using %dMB",$i,$k,$n,memory_get_usage()/1048576));
-				}
-			}
-			if ($i > 0) {
-				$pricelist->setUpdatedAt(new \DateTime());
-				$pricelistManager->save($pricelist);
-			}
-			$pricelistObjectManager->clear('ProductPrice');
-		}
+            if ((($i > $iprec ) && ($i % 500) === 0) || ($k == $n)) {
+                $iprec = $i;
+                $this->pricelistManager->getObjectManager()->flush();
+                $this->output->writeln(sprintf("Updated %d uom of %d/%d prices using %dMB",$i,$k,$n,memory_get_usage()/1048576));
+            }
+        }
+        if ($i > 0) {
+            $pricelist->setUpdatedAt(new \DateTime());
+            $this->pricelistManager->save($pricelist);
+        }
+        $this->pricelistManager->getObjectManager()->clear();
 	}
 	
 }
