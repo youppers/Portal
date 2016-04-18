b234d1ea6783c471b3c6c53ad77e9fb0e383bd1a
sergio xubuntu copper <sergio.strampelli@youppers.com>
2016-04-18 23:19:37 +0200

    Cristina rubinetterie loader: uses 2 traling chars for serie

--- youppers/src/Youppers/CompanyBundle/Loader/CRS/PricelistLoader.php
+++ youppers/src/Youppers/CompanyBundle/Loader/CRS/PricelistLoader.php
@@ -13,7 +13,14 @@ class PricelistLoader extends AbstractPricelistLoader
 		$mapping = array(
 			self::FIELD_CODE => 'Nr. articolo',
 			self::FIELD_NAME => 'DEC ITA',
-			self::FIELD_COLLECTION => 'SERIE',
+			self::FIELD_COLLECTION => function ($row) {
+				if (preg_match('/^[A-Z]{5}[0-9]{5}([A-Z]{2})$/',$row['Nr. articolo'],$matches)) {
+					return $matches[1];
+				} else {
+					return $row['SERIE'];
+				}
+
+			},
 			self::FIELD_PRICE => 'L 2015/16',
 		);
 		$mapper = new LoaderMapper($mapping);
