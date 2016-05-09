8418d7fd84d3c06e367f88d8098b55d114c2b99e
sergio xubuntu copper <sergio.strampelli@youppers.com>
2016-05-09 22:08:42 +0200

    link to pricelist from company show

--- youppers/src/Youppers/CompanyBundle/Admin/CompanyAdmin.php
+++ youppers/src/Youppers/CompanyBundle/Admin/CompanyAdmin.php
@@ -59,6 +59,7 @@ class CompanyAdmin extends YouppersAdmin
                  ),
 				'associated_property' => 'nameCodeStatus'
              ))
+			->add('pricelists', null, array('route' => array('name' => 'show'),'associated_property' => 'code'))
 		;
 	}
 
